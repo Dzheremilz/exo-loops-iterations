b15c7a08418257d3e13d920c2b332d767c2bbf62
Dzheremilz <trempejeremie@gmail.com>
2021-02-26 18:27:22 +0100

    add method 2 & 3

--- sumAll3D.js
+++ sumAll3D.js
@@ -23,3 +23,60 @@ for (const elem of tab) {
   }
 }
 console.log(x)
+
+/* V2
+let tab = [
+  [
+    [1, 7, 3],
+    [11, 17, 7],
+    [-3, -5],
+    [5, 13],
+  ],
+  [
+    [2, 4, 6, 8, 10],
+    [1, 3, 5],
+  ],
+  [[0]],
+  [[0], [1], [2], [1]],
+]
+
+let x = 0
+
+for (let i = 0; i < tab.length; i++) {
+  for (let j = 0; j < tab[i].length; j++) {
+    for (let k = 0; k < tab[i][j].length; k++) {
+      x += tab[i][j][k]
+    }
+  }
+}
+
+console.log(x)
+
+*/
+
+/* V3
+
+let tab = [
+  [
+    [1, 7, 3],
+    [11, 17, 7],
+    [-3, -5],
+    [5, 13],
+  ],
+  [
+    [2, 4, 6, 8, 10],
+    [1, 3, 5],
+  ],
+  [[0]],
+  [[0], [1], [2], [1]],
+]
+
+let x = 0
+
+for (let i = 0; i < tab.length; i++) {
+  for (let j = 0; j < tab[i].length; j++) {
+    x += tab[i][j].reduce((a, b) => { return a + b })
+  }
+}
+
+*/
